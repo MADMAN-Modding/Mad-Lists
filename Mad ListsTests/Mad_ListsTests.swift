c511a4819dedf9c8dbb8fd98caa99a19ded03dca
MADMAN-Modding <mad@kmdtech.net>
2025-12-24 14:04:20 -0500

    Initial Commit

---
+++ Mad ListsTests/Mad_ListsTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mad_ListsTests.swift
+//  Mad ListsTests
+//
+//  Created by Mason Dehl on 12/24/25.
+//
+
+import Testing
+
+struct Mad_ListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
